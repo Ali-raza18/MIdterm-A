5eaab9c464d8592ee4b288a8dc567030dddb54bb
Ali-raza18 <ali.raza.rizvi.2209@gmail.com>
2024-12-04 13:17:31 +0500

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double=1000;
+	do{
+		cout << "welcome to ATM/n" << endl;
+		cout <<"1.check balance/n" <<endl;
+	    cout <<"2.deposit money/n" <<endl;
+	    cout <<"3.withdraw money/n"<<endl;
+	    cout <<"4.Exit." <<endl;
+	 }
+    cin>>choice
+    while{
+	switch(choice);
+	choice 1:
+		cout<< "i" << balance amount<<endl;
+    break;
+	choice 2:
+        cout<<"enter the amount you want to deposit";
+        cin>>amount
+        cout<<"current balance is." << i+=amount<<endl;
+        break;
+    choice 3:
+    	cout<<"enter amount you want to withdraw";
+    	cin>>amount;
+    	if(amount<=balance)
+    	 cout << "withdrawing your amount" << your current balance is << i-=amount<<endl;
+       else
+       cout<< "insufficent amount,re enter your amount";
+       break;
+       choice 4 
+       cout << "Exiting.."
+        
+       {
+		 while
+        (choice !=4)
+        cout<<"Thanks for using ATM."
+}
+		}
+		}
+		
+		
